d8f445d2c0949c7dcf780af277f05803c4e90203
jjean-v <14jean04@gmail.com>
2026-09-14 10:39:05 +0200

    dev: exo 1 finish

--- tp2/cmd/1-compte/compte.go
+++ tp2/cmd/1-compte/compte.go
@@ -12,17 +12,38 @@ func compte(n int) {
 func compteWithGoRoutine(n int) {
 	for i :=0; i < n; i++ {
 
-		go Print(i)
-
-		
+		go fmt.Println(i)
 	}
 }
 
-func Print(i int) {
-	fmt.Println(i)
+
+func compteMsg(n int, msg string) {
+	for i :=0; i < n; i++ {
+		fmt.Println(msg)
+		fmt.Println(i)	
+	}
+}
+
+func compteMsgFromTo(start int, end int, msg string) {
+	for i := start; i < end; i++ {
+		fmt.Println(msg, i)
+	}
+}
+
+func testCompteMsg() {
+	go compteMsg(5, "func1")
+	go compteMsg(5, "func2")
+}
+
+func testCompteMsgFromTo(start int, end int, msg string) {
+	for i := 0; i <= 90; i +=10 {
+		go compteMsgFromTo(i,i+10,"hey")
+	}
 }
 
 func main() {
-	compteWithGoRoutine(10)
+	
+	//compteWithGoRoutine(5)
+	testCompteMsg()
 	fmt.Scanln()
 }
